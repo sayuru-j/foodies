e6889762831d90b4458a360ec2d4ebec4ccb70a2
Sayuru <zayuru.ai@gmail.com>
2023-05-09 16:52:19 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import Navigation from "./pages/Routes/Routes";
 
 function App() {
+  useEffect(() => {
+    function handleContextMenu(event) {
+      event.preventDefault();
+    }
+
+    const images = document.querySelectorAll("img");
+    images.forEach((image) => {
+      image.addEventListener("contextmenu", handleContextMenu);
+    });
+
+    return () => {
+      images.forEach((image) => {
+        image.removeEventListener("contextmenu", handleContextMenu);
+      });
+    };
+  }, []);
   return (
     <>
       <Navigation />
