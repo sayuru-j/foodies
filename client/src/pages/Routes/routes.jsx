561a694d9bc1bdaf45c06f6a5fa5eb13e26c4b5f
Sayuru <zayuru.ai@gmail.com>
2023-05-11 02:46:15 +0530

    Update routes.jsx

--- client/src/pages/Routes/routes.jsx
+++ client/src/pages/Routes/routes.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Home/home";
 import Login from "../../components/Login/Login";
 import AddPost from "../../components/Post/AddPost";
+import Notification from "../../components/Notification/Notification";
+import Profile from "../../components/Profile/Profile";
+import Settings from "../../components/Settings/Settings";
+import Search from "../../components/Search/Search";
 
 function Navigation() {
   return (
@@ -9,6 +13,10 @@ function Navigation() {
       <Route path="/login" exact Component={Login} />
       <Route path="/" element={<Home />} />
       <Route path="/addpost" Component={AddPost} />
+      <Route path="/notification" element={<Notification />} />
+      <Route path="/profile/my" Component={Profile} />
+      <Route path="/search" Component={Search} />
+      <Route path="/settings" Component={Settings} />
     </Routes>
   );
 }
