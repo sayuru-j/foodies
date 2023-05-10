cf5d702b850696a81a41a06c56145cb742c5ea45
Sayuru <zayuru.ai@gmail.com>
2023-05-11 02:45:59 +0530

    Update Bottombar.jsx

--- client/src/components/Sidebar/Bottombar.jsx
+++ client/src/components/Sidebar/Bottombar.jsx
@@ -5,8 +5,18 @@ import {
   SearchIcon,
 } from "@heroicons/react/solid";
 import NoContextMenuImage from "../../helpers/NoContextMenuImage";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function Bottombar({ avatar }) {
+function Bottombar() {
+  const [loginDetails, setLoginDetails] = useState([]);
+
+  useEffect(() => {
+    let loginDetails = localStorage.getItem("loginDetails");
+    const accessToken = JSON.parse(loginDetails)?.accessToken;
+
+    setLoginDetails(JSON.parse(loginDetails));
+  }, []);
   return (
     <div className="fixed w-full md:hidden bg-white flex bottom-0 z-50 py-2 items-center justify-between border-t-[1px] shadow-sm px-10">
       <div className="flex gap-2 lg:justify-normal justify-center items-center hover:bg-slate-200 rounded-md p-2">
@@ -25,7 +35,7 @@ function Bottombar({ avatar }) {
       <div className="flex gap-2 lg:justify-normal justify-center items-center hover:bg-slate-200 rounded-md p-2">
         <NoContextMenuImage
           className="w-7 h-7 object-cover rounded-full"
-          src={avatar}
+          src={loginDetails?.user?.avatar}
           alt=""
         />
       </div>
