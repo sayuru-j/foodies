9cf6c7afe60d463b7c4a457a8c7352b6d2e46cd7
Sayuru <zayuru.ai@gmail.com>
2023-05-11 02:46:04 +0530

    Update Timeline.jsx

--- client/src/components/Timeline/Timeline.jsx
+++ client/src/components/Timeline/Timeline.jsx
@@ -1,10 +1,13 @@
 import MiniProfile from "../MiniProfile/MiniProfile";
 import Post from "../Post/Post";
 import Stories from "../Stories/Stories";
+import { Gradient } from "../../constants/Gradients";
 
 function Timeline() {
   return (
-    <div className="lg:ml-64 md:ml-16 flex flex-col gap-2">
+    <div
+      className={`${Gradient} min-h-screen lg:ml-64 md:ml-16 flex flex-col gap-2`}
+    >
       <div className="lg:grid lg:grid-cols-3 grid-cols-1">
         <div className="flex flex-col gap-4 col-span-2 px-2 pb-32">
           <Stories />
