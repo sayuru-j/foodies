b03ceec76edcc06b5283125ca54c865d6e6fa044
Sayuru <zayuru.ai@gmail.com>
2023-05-11 14:02:02 +0530

    Update home.jsx

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -1,33 +1,13 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Bottombar from "../../components/Sidebar/Bottombar";
 import Timeline from "../../components/Timeline/Timeline";
-import LoginToProceed from "../../components/LoginToProceed/LoginToProceed";
-import { useState } from "react";
-import { useEffect } from "react";
 
 function Home() {
-  const [accessToken, setAccessToken] = useState(null);
-  const [loginDetails, setLoginDetails] = useState([]);
-
-  useEffect(() => {
-    let loginDetails = localStorage.getItem("loginDetails");
-    const accessToken = JSON.parse(loginDetails)?.accessToken;
-
-    setLoginDetails(JSON.parse(loginDetails));
-    setAccessToken(accessToken);
-  }, []);
-
   return (
     <>
-      {accessToken ? (
-        <>
-          <Sidebar />
-          <Bottombar />
-          <Timeline />
-        </>
-      ) : (
-        <LoginToProceed />
-      )}
+      <Sidebar />
+      <Bottombar />
+      <Timeline />
     </>
   );
 }
