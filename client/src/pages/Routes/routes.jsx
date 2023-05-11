70482dc5b26a8d28157a0d5c7fa2988bb8346431
Sayuru <zayuru.ai@gmail.com>
2023-05-11 14:02:03 +0530

    Update routes.jsx

--- client/src/pages/Routes/routes.jsx
+++ client/src/pages/Routes/routes.jsx
@@ -6,17 +6,43 @@ import Notification from "../../components/Notification/Notification";
 import Profile from "../../components/Profile/Profile";
 import Settings from "../../components/Settings/Settings";
 import Search from "../../components/Search/Search";
+import { useState } from "react";
+import { useEffect } from "react";
+import LoginToProceed from "../../components/LoginToProceed/LoginToProceed";
 
 function Navigation() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [loginDetails, setLoginDetails] = useState([]);
+
+  useEffect(() => {
+    let loginDetails = localStorage.getItem("loginDetails");
+    const accessToken = JSON.parse(loginDetails)?.accessToken;
+
+    setLoginDetails(JSON.parse(loginDetails));
+    setAccessToken(accessToken);
+  }, []);
   return (
     <Routes>
       <Route path="/login" exact Component={Login} />
-      <Route path="/" element={<Home />} />
-      <Route path="/addpost" Component={AddPost} />
-      <Route path="/notification" element={<Notification />} />
-      <Route path="/profile/my" Component={Profile} />
-      <Route path="/search" Component={Search} />
-      <Route path="/settings" Component={Settings} />
+
+      <Route path="/" element={accessToken ? <Home /> : <LoginToProceed />} />
+      <Route
+        path="/addpost"
+        Component={accessToken ? AddPost : LoginToProceed}
+      />
+      <Route
+        path="/notification"
+        Component={accessToken ? Notification : LoginToProceed}
+      />
+      <Route
+        path="/profile/my"
+        Component={accessToken ? Profile : LoginToProceed}
+      />
+      <Route path="/search" Component={accessToken ? Search : LoginToProceed} />
+      <Route
+        path="/settings"
+        Component={accessToken ? Settings : LoginToProceed}
+      />
     </Routes>
   );
 }
