d5ab3957b3ec57fa3a9c4ddb477f8b8b1dc627c1
Sayuru <zayuru.ai@gmail.com>
2023-05-11 14:01:59 +0530

    Update Settings.jsx

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,14 +1,32 @@
+import { GoogleLogout } from "react-google-login";
 import { Gradient } from "../../constants/Gradients";
+import { useNavigate } from "react-router-dom";
 
 function Settings() {
+  const navigate = useNavigate();
+
+  const onLogoutSuccess = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
   return (
     <div
       className={`${Gradient} flex flex-col gap-1 w-full h-screen items-center justify-center`}
     >
       <h1 className="font-medium">Warning!</h1>
-      <button className="bg-secondary py-1 px-4 text-white font-bold rounded-md">
-        Delete Account
-      </button>
+
+      <GoogleLogout
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            className="bg-secondary py-1 px-4 text-white font-bold rounded-md"
+          >
+            Delete Account
+          </button>
+        )}
+        clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+        onLogoutSuccess={onLogoutSuccess}
+      />
     </div>
   );
 }
