605e6cecb31492939541cd8adda12290af8d963e
Sayuru <zayuru.ai@gmail.com>
2023-05-12 02:01:31 +0530

    Update UserRepo.java

--- server/src/main/java/com/example/demonew/repo/UserRepo.java
+++ server/src/main/java/com/example/demonew/repo/UserRepo.java
@@ -3,6 +3,6 @@ package com.example.demonew.repo;
 import com.example.demonew.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long>{
+public interface UserRepo extends JpaRepository<User, Integer>{
     User findByUsername(String username);
 }
