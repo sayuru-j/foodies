3a42ecff3b8a40887d269614d2ee886a5b4bc279
Sayuru <zayuru.ai@gmail.com>
2023-05-14 13:11:30 +0530

    Create CommentController.java

---
+++ server/src/main/java/com/example/demonew/controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.example.demonew.controller;
+
+import com.example.demonew.dto.CommentDTO;
+import com.example.demonew.dto.PostDTO;
+import com.example.demonew.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/comment")
+@CrossOrigin
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    // Request for get all comments
+    @GetMapping("/getComments")
+    public List<CommentDTO> getCommets(){
+        return commentService.getAllComments();
+
+    }
+
+    // Request for save a comment
+    @PostMapping("/savePost")
+    public CommentDTO savePost(@RequestBody CommentDTO commentDTO){
+        return commentService.saveComment(commentDTO);
+
+
+    }
+
+    // Request for update a comment
+    @PutMapping("/updateComment/{commentid}")
+    public CommentDTO updateComment(@PathVariable int commentid, @RequestBody CommentDTO commentDTO){
+        return commentService.updateComment(commentid, commentDTO);
+
+
+    }
+
+}
