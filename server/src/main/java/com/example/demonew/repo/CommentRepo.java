d8d6bd3693c650e15ef077bff8a3b2df87fd296b
Sayuru <zayuru.ai@gmail.com>
2023-05-16 20:59:58 +0530

    Update CommentRepo.java

--- server/src/main/java/com/example/demonew/repo/CommentRepo.java
+++ server/src/main/java/com/example/demonew/repo/CommentRepo.java
@@ -2,7 +2,11 @@ package com.example.demonew.repo;
 
 import com.example.demonew.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepo extends JpaRepository<Comment, Integer> {
-
+    @Modifying
+    @Query("delete from Comment c where c.commentid = ?1")
+    void deleteByCommentid(int commentid);
 }
